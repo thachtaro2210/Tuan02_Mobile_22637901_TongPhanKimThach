77e451c5ecb05abe5a3d1cb9fd8470e73b5890e8
H82M19\Student <Student>
2025-08-30 14:22:14 +0700

    Complete Bai02 in asyncawait with array number

--- src/AsyncAwait/Bai02.ts
+++ src/AsyncAwait/Bai02.ts
@@ -1,8 +1,16 @@
 export async function excercise2(){
-    function simulateTask(ms: number) {
-    return new Promise(resolve => setTimeout(() => resolve(`Hello Async bai 02 :${ms}`), ms));
+    function simulateTask(ms: number[]) {
+    return new Promise(async resolve => {
+        for (const t of ms){
+            await new  Promise(r => setTimeout(r,t));
+            console.log(`Helo bai 2 :${t}` );
+            
+        }
+        resolve("Done");
+    }
+    );
   }
 
-  const result = await simulateTask(2000);
+  const result = await simulateTask([100,299,300]);
   console.log(result);
 }
