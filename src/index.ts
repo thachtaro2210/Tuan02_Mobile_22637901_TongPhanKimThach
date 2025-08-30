7bdf7965cfb033c28bab6104e696ac5a73c8a74f
H82M19\Student <Student>
2025-08-30 13:14:38 +0700

    Complete Bai05 in basics with promise

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { helloAsync } from "./Basics with Promise/Bai1";
 import { getTenAfterOneSecond } from "./Basics with Promise/Bai2";
 import { rejectPromiseAfterOneSecond } from "./Basics with Promise/Bai3";
 import { getRandomNumber } from "./Basics with Promise/Bai4";
+import { simulateTask } from "./Basics with Promise/Bai5";
 
 
 helloAsync.then((message : any) => {
@@ -24,3 +25,5 @@ rejectPromiseAfterOneSecond().catch((error: any) => {
 //Bai04
 
 getRandomNumber();
+//Bai05
+simulateTask(1000).then(console.log); 
